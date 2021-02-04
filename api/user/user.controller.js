3a25d43d2dbf8bf0b47ab26a283f4d1f095008b6
Aryan-sws <mrwhite21@outlook.com>
2021-02-04 21:52:02 +0530

    bycript password

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -1,15 +1,23 @@
 const user = require('./user.module');
+const bcrypt = require('bcrypt');
 
 module.exports = {
    signup:async (req,res)=>{
        try{
-          let User = new user(req.body);
-          let result = await User.save();
-          if(result){
-             res.status(200).send({result: true,data: result});
-          } else{
-            res.status(404).send({result: false,data: '', message: "no data found"});
-          }
+         let isUserExist = await user.findOne({email:req.body.email});
+         if(isUserExist){
+            res.status(409).send({result: false,data: '', message: "user is already exist"});
+         } else{
+            let User = new user(req.body);
+            let salt = await bcrypt.genSalt(10);
+            User.password = await bcrypt.hash(User.password, salt);
+            let result = await User.save();
+            if(result){
+               res.status(200).send({result: true,data: result});
+            } else{
+              res.status(404).send({result: false,data: '', message: "no data found"});
+            }
+         }
        }catch(err){
            console.log(err.message);
        }
@@ -18,7 +26,8 @@ module.exports = {
       try{
           let isUser = await user.findOne({email:req.body.email});
            if(isUser){
-              if(isUser.password == req.body.password){
+            let isValidPassword = await bcrypt.compare(req.body.password, isUser.password);
+              if(isValidPassword){
                  res.status(200).send({result: true, data: isUser})
               } else{
                  res.status(404).send({result: false, data: '', message:"Please check  password"})
